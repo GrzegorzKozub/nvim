f29dd674f532b91ccef303b3c9749073c91e9bc0
greg <grzegorz.kozub@gmail.com>
2022-12-24 16:28:40 +0100

    Fixed keys

--- lua/cfg/keys.lua
+++ lua/cfg/keys.lua
@@ -3,21 +3,20 @@ local M = {}
 local options = { noremap = true, silent = true }
 
 local function neovim()
+  -- delete with backspace in visual mode
   vim.keymap.set("v", "<BS>", "d", options)
+
+  -- visual block (not needed on linux)
+  -- vim.keymap.set("", "<C-Q>", "<C-V>", options)
+
+  -- select all
   vim.keymap.set("n", "<C-A>", "ggVG", options)
-  if vim.fn.has("clipboard") then
-    --vnoremap <C-C> "+y
-    --vnoremap <C-X> "+x
-    --map <C-V> "+gP
-    --cmap <C-V> <C-R>+
-    --exe 'inoremap <script> <C-V> <C-G>u' . paste#paste_cmd['i']
-    --exe 'vnoremap <script> <C-V> ' . paste#paste_cmd['v']
-    vim.keymap.set("v", "<C-C>", '"+y', options)
-    vim.keymap.set("v", "<C-X>", '"+x', options)
-    vim.keymap.set({ "n", "i", "v" }, "<C-V>", '"+gP', options)
-    vim.keymap.set("c", "<C-V>", "<C-R>+", options)
-  end
-  vim.keymap.set("", "<C-Q>", "<C-V>", options)
+
+  -- clipboard
+  vim.keymap.set("v", "<C-C>", '"+y', options)
+  vim.keymap.set("v", "<C-X>", '"+x', options) -- conflicts with tmux
+  vim.keymap.set({ "n", "v", "x" }, "<C-V>", '"+gP', options)
+  vim.keymap.set({ "c", "i" }, "<C-V>", "<C-R>+", { noremap = true })
 end
 
 local function plugins()
