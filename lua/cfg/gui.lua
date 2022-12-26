d13d80d28456c5dfaaafb58e9b33540ff4401eed
greg <grzegorz.kozub@gmail.com>
2022-12-26 13:01:03 +0100

    Migrate gui settings

---
+++ lua/cfg/gui.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.setup()
+  local options = { noremap = true, silent = true }
+
+  vim.keymap.set('n', '<F10>', function()
+    vim.fn.GuiWindowMaximized(vim.g.GuiWindowMaximized == 0)
+  end, options)
+
+  vim.keymap.set('n', '<F11>', function()
+    vim.fn.GuiWindowFullScreen(vim.g.GuiWindowFullScreen == 0)
+  end, options)
+
+  vim.cmd.Guifont 'Fira Code Retina:h12'
+  vim.cmd.GuiRenderLigatures(1)
+end
+
+return M
