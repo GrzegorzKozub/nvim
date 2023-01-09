e512e4eda7718d1dbcf68c9390faf5372d03682d
greg <grzegorz.kozub@gmail.com>
2023-01-09 20:59:46 +0100

    Configure lsp and diagnistic keys

--- lua/cfg/lsp/nvim-lspconfig.lua
+++ lua/cfg/lsp/nvim-lspconfig.lua
@@ -9,19 +9,23 @@ local function keys(bufnr)
   map('gD', vim.lsp.buf.declaration, bufnr)
   map('gi', vim.lsp.buf.implementation, bufnr)
   map('gr', vim.lsp.buf.references, bufnr)
-  map('K', vim.lsp.buf.hover, bufnr)
 
-  -- continue:
-  map('gl', vim.diagnostic.open_float, bufnr)
-  map('<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>', bufnr)
-  map('<leader>li', '<cmd>LspInfo<cr>', bufnr)
-  map('<leader>lI', '<cmd>Mason<cr>', bufnr)
-  map('<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', bufnr)
-  map('<leader>lj', '<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>', bufnr)
-  map('<leader>lk', '<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>', bufnr)
-  map('<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>', bufnr)
-  map('<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>', bufnr)
-  map('<leader>lq', '<cmd>lua vim.diagnostic.setloclist()<cr>', bufnr)
+  map('<leader>lh', vim.lsp.buf.hover, bufnr)
+  map('<leader>ls', vim.lsp.buf.signature_help, bufnr)
+
+  map('<leader>lr', vim.lsp.buf.rename, bufnr)
+  map('<leader>la', vim.lsp.buf.code_action, bufnr)
+  map('<leader>lf', function()
+    vim.lsp.buf.format { async = true }
+  end, bufnr)
+
+  map('<leader>dh', vim.diagnostic.open_float, bufnr)
+
+  map('<leader>dl', vim.diagnostic.setloclist, bufnr)
+  map('<leader>dq', vim.diagnostic.setqflist, bufnr)
+
+  map(']d', vim.diagnostic.goto_next, bufnr)
+  map('[d', vim.diagnostic.goto_prev, bufnr)
 end
 
 local function on_attach(_, bufnr)
