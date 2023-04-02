011d9cee3f0f5be58844d26aabcc8644fdf8a8b5
greg <grzegorz.kozub@gmail.com>
2023-04-02 13:47:47 +0200

    fix vi mode cursor in windows terminal when leaving nvim

--- lua/cfg/autocmds.lua
+++ lua/cfg/autocmds.lua
@@ -32,6 +32,19 @@ local function last_seen_location()
   })
 end
 
+local function wt_cursor()
+  if vim.fn.has 'win32' == 0 then return end
+  vim.api.nvim_create_autocmd('VimLeave', {
+    pattern = '*',
+    callback = function()
+      -- https://github.com/neovim/neovim/issues/4396
+      vim.opt.guicursor = ''
+      vim.fn.chansend(vim.v.stderr, '\x1b[ q')
+    end,
+    group = vim.api.nvim_create_augroup('WindowsTerminalCursor', { clear = true }),
+  })
+end
+
 local function yank_highlight()
   vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
@@ -45,6 +58,7 @@ end
 function M.config()
   file_types()
   last_seen_location()
+  wt_cursor()
   yank_highlight()
 end
 
