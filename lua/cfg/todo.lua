5167baf9f165779fc464dd3b49ce5b9acbd41413
greg <grzegorz.kozub@gmail.com>
2023-09-19 18:02:50 +0200

    finished markdown todo

--- lua/cfg/todo.lua
+++ lua/cfg/todo.lua
@@ -1,69 +1,66 @@
-local ts = require('nvim-treesitter.ts_utils')
-
 local M = {}
-local U = {}
 
-U.find_parent_node = function(node, type)
-  if (node == node:root()) then return nil end
-  if (node:type() == type) then return node end
-  return U.find_parent_node(node:parent(), type)
+local function find_parent_node(node, type)
+  if node == node:root() then
+    return nil
+  end
+  if node:type() == type then
+    return node
+  end
+  return find_parent_node(node:parent(), type)
 end
 
-local find_child_node = function(node, type)
+local function find_child_node(node, type)
   local child = node:child(0)
   while child do
-    if (child:type() == type) then return child end
+    if child:type() == type then
+      return child
+    end
     child = child:next_sibling()
   end
   return nil
 end
 
-local set_node_text = function(node, text, bufnr)
+local function set_node_text(node, text)
   local sr, sc, er, ec = node:range()
-  local content = { text }
-  if (type(text) == 'table') then content = text end
-  vim.api.nvim_buf_set_text(bufnr or 0, sr, sc, er, ec, content)
+  vim.api.nvim_buf_set_text(0, sr, sc, er, ec, (type(text) == 'table' and text or { text }))
 end
 
-local get_node_text = function(node, bufnr)
+local function get_node_text(node)
   local sr, sc, er, ec = node:range()
-  local text = vim.api.nvim_buf_get_text(bufnr or 0, sr, sc, er, ec, {})
-  if (#text == 1) then return text[1] end
-  return text
+  local text = vim.api.nvim_buf_get_text(0, sr, sc, er, ec, {})
+  return (#text == 1 and text[1] or text)
 end
 
-function M.toggle()
-  local node = ts.get_node_at_cursor(nil, true)
-  local item = U.find_parent_node(node, 'list_item')
-  if (not item) then
+local function toggle()
+  local node = require('vim.treesitter').get_node()
+  local item = find_parent_node(node, 'list_item')
+  if not item then
     return
   end
-
   local unchecked = find_child_node(item, 'task_list_marker_unchecked')
   if unchecked then
     set_node_text(unchecked, '[x]')
     return
   end
   local checked = find_child_node(item, 'task_list_marker_checked')
-  local box = checked
-
-  if box then
-    set_node_text(box, {})
+  if checked then
+    set_node_text(checked, nil)
     local marker = item:child()
     set_node_text(marker, get_node_text(marker):sub(1, 1))
     return
   end
-
   local content = find_child_node(item, 'paragraph')
-  if not content then return end
+  if not content then
+    return
+  end
   local text = get_node_text(content)
-
   text[1] = '[ ] ' .. text[1]
   set_node_text(content, text)
 end
 
 function M.config()
-  require('cfg.util').nmap('<leader>x', M.toggle)
+  require('cfg.util').nmap('<leader>x', toggle)
 end
 
 return M
