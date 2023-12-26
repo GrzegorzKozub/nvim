92aea1fba68640e4253096370d35902aff642453
greg <grzegorz.kozub@gmail.com>
2023-12-26 09:18:10 +0100

    move cmp icons to icons.lua

--- lua/cfg/icons.lua
+++ lua/cfg/icons.lua
@@ -2,6 +2,33 @@ local M = {}
 
 function M.get()
   return {
+    cmp = {
+      class = '󰠱',
+      color = '',
+      constant = '󰏿',
+      constructor = '󰙴',
+      enum = '󱍶',
+      enum_member = '󱍶',
+      event = '󰛕',
+      field = '󰌖',
+      file = '󰈤',
+      folder = '󰉖',
+      func = '󰊕',
+      interface = '',
+      keyword = '󰌆',
+      method = '󰅲',
+      module = '󰅩',
+      operator = '󰆕',
+      property = '󰓼',
+      reference = '󰌷',
+      snippet = '󰑷',
+      struct = '󰙅',
+      text = '󰦨',
+      type_parameter = '󰅴',
+      unit = '󰑭',
+      value = '󰎠',
+      variable = '󰀫',
+    },
     diag = {
       error = '󰅙 ', -- ●
       hint = '󰋼 ', -- ◆
