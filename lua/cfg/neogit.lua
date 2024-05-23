abde21b3287aeffc44035a47d2b694961d3067b6
greg <grzegorz.kozub@gmail.com>
2024-05-23 07:50:33 +0200

    neogit is not documented

--- lua/cfg/neogit.lua
+++ lua/cfg/neogit.lua
@@ -1,12 +1,12 @@
-local M = {}
-
-function M.config()
-  local neogit_loaded, neogit = pcall(require, 'neogit')
-  if not neogit_loaded then
-    return
-  end
-
-  neogit.setup {}
-end
-
-return M
+-- local M = {}
+--
+-- function M.config()
+--   local neogit_loaded, neogit = pcall(require, 'neogit')
+--   if not neogit_loaded then
+--     return
+--   end
+--
+--   neogit.setup {}
+-- end
+--
+-- return M
--- lua/plg/neogit.lua
+++ lua/plg/neogit.lua
@@ -1,10 +1,10 @@
 return {
-  'NeogitOrg/neogit',
-  cmd = 'Neogit',
-  config = require('cfg.neogit').config,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    -- 'sindrets/diffview.nvim',
-  },
+  -- 'NeogitOrg/neogit',
+  -- cmd = 'Neogit',
+  -- config = require('cfg.neogit').config,
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'nvim-telescope/telescope.nvim',
+  --   -- 'sindrets/diffview.nvim',
+  -- },
 }
