a631dfd7982fdd63bdd17c778419d9345217e376
greg <grzegorz.kozub@gmail.com>
2024-06-15 09:36:59 +0200

    trouble simplify

--- lua/cfg/trouble.lua
+++ lua/cfg/trouble.lua
@@ -6,36 +6,63 @@ function M.config()
     return
   end
 
+  local file_icon = '{hl:TroubleIconFile}{file_icon}{hl}'
+
   local icons = require('cfg.icons').get()
 
-  trouble.setup {
+  local kinds = {
+    Array = icons.dev.array,
+    Boolean = icons.dev.boolean,
+    Class = icons.dev.class,
+    Constant = icons.dev.constant,
+    Constructor = icons.dev.constructor,
+    Enum = icons.dev.enum,
+    EnumMember = icons.dev.enum_member,
+    Event = icons.dev.event,
+    Field = icons.dev.field,
+    File = icons.file.file,
+    Function = icons.dev.func,
+    Interface = icons.dev.interface,
+    Key = icons.dev.keyword,
+    Method = icons.dev.method,
+    Module = icons.dev.module,
+    Namespace = icons.dev.namespace,
+    Null = icons.dev.null,
+    Number = icons.dev.number,
+    Object = icons.dev.object,
+    Operator = icons.dev.operator,
+    Package = icons.dev.package,
+    Property = icons.dev.property,
+    String = icons.dev.string,
+    Struct = icons.dev.struct,
+    TypeParameter = icons.dev.type_parameter,
+    Variable = icons.dev.variable,
+  }
+
+  -- for kind, _ in pairs(kinds) do
+  --   kinds[kind] = kinds[kind] .. ' '
+  -- end
+
+  local config = {
     modes = {
       diagnostics = {
         groups = {
-          { 'directory', format = ' {directory_icon}{directory} {count}' },
-          { 'filename', format = '{hl:TroubleIconFile}{file_icon}{hl}{basename} {count}' },
+          { 'directory', format = ' {directory_icon}{directory}{count}' },
+          { 'filename', format = file_icon .. '{basename}{count}' },
         },
         format = '{severity_icon} {message:md} {item.source} '
           .. '{hl:TroubleSource}({hl}{code}{hl:TroubleSource}){hl} {pos}',
         win = { size = { height = 16 } },
       },
       lsp_document_symbols = {
-        title = ' {hl:TroubleTitle}symbols{hl} {count}',
-        groups = { { 'filename', format = '{hl:TroubleIconFile}{file_icon}{hl}{filename} {count}' } },
-        format = '{kind_icon}{symbol.name} {text:Comment} {pos}',
+        title = ' {hl:TroubleTitle}symbols{hl}{count}',
+        groups = { { 'filename', format = file_icon .. '{filename}{count}' } },
         win = { size = 32 },
       },
       lsp_base = {
-        groups = { { 'filename', format = '{hl:TroubleIconFile}{file_icon}{hl}{filename} {count}' } },
+        groups = { { 'filename', format = file_icon .. '{filename}{count}' } },
         format = '{text:ts} {hl:TroubleSource}({item.client}){hl} {pos}',
       },
-      lsp_definitions = { title = ' {hl:TroubleTitle}definitions{hl} {count}' },
-      lsp_references = { title = ' {hl:TroubleTitle}references{hl} {count}' },
-      lsp_implementations = { title = ' {hl:TroubleTitle}implementations{hl} {count}' },
-      lsp_type_definitions = { title = ' {hl:TroubleTitle}type definitions{hl} {count}' },
-      lsp_declarations = { title = ' {hl:TroubleTitle}declarations{hl} {count}' },
-      lsp_incoming_calls = { title = ' {hl:TroubleTitle}incoming calls{hl} {count}' },
-      lsp_outgoing_calls = { title = ' {hl:TroubleTitle}outgoing calls{hl} {count}' },
       lsp = { win = { size = { height = 16 } } },
     },
     icons = {
@@ -44,36 +71,25 @@ function M.config()
         fold_open = icons.fold.open,
         last = icons.fold.guides.last,
       },
-      kinds = {
-        Array = icons.dev.array .. ' ',
-        Boolean = icons.dev.boolean .. ' ',
-        Class = icons.dev.class .. ' ',
-        Constant = icons.dev.constant .. ' ',
-        Constructor = icons.dev.constructor .. ' ',
-        Enum = icons.dev.enum .. ' ',
-        EnumMember = icons.dev.enum_member .. ' ',
-        Event = icons.dev.event .. ' ',
-        Field = icons.dev.field .. ' ',
-        File = icons.file.file .. ' ',
-        Function = icons.dev.func .. ' ',
-        Interface = icons.dev.interface .. ' ',
-        Key = icons.dev.keyword .. ' ',
-        Method = icons.dev.method .. ' ',
-        Module = icons.dev.module .. ' ',
-        Namespace = icons.dev.namespace .. ' ',
-        Null = icons.dev.null .. ' ',
-        Number = icons.dev.number .. ' ',
-        Object = icons.dev.object .. ' ',
-        Operator = icons.dev.operator .. ' ',
-        Package = icons.dev.package .. ' ',
-        Property = icons.dev.property .. ' ',
-        String = icons.dev.string .. ' ',
-        Struct = icons.dev.struct .. ' ',
-        TypeParameter = icons.dev.type_parameter .. ' ',
-        Variable = icons.dev.variable .. ' ',
-      },
+      kinds = kinds,
     },
   }
+
+  for _, mode in ipairs {
+    'definitions',
+    'references',
+    'implementations',
+    'type_definitions',
+    'declarations',
+    'incoming_calls',
+    'outgoing_calls',
+  } do
+    config.modes['lsp_' .. mode] = {
+      title = ' {hl:TroubleTitle}' .. mode .. '{hl}{count}',
+    }
+  end
+
+  trouble.setup(config)
 end
 
 return M
