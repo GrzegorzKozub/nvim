293fc499342982a07d91d2a1963758cbe51393cb
greg <grzegorz.kozub@gmail.com>
2024-09-27 08:25:24 +0200

    remark vscode-neovim

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require('cfg.options').config()
 require('cfg.keys').config()
 require('cfg.yank-highlight').config()
 
-if not vim.g.vscode then
+if not vim.g.vscode then -- https://github.com/vscode-neovim/vscode-neovim
   require('cfg.file-types').config()
   require('cfg.markdown').config()
   require('cfg.resume').config()
