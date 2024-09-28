796a1dfcbd19ac7f078cc043b086b505b07c87cb
greg <grzegorz.kozub@gmail.com>
2024-09-28 07:33:51 +0200

    drop vscode extension

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 require('cfg.options').config()
 require('cfg.keys').config()
-require('cfg.yank-highlight').config()
 
-if not vim.g.vscode then -- https://github.com/vscode-neovim/vscode-neovim
-  require('cfg.file-types').config()
-  require('cfg.markdown').config()
-  require('cfg.resume').config()
-  require('cfg.wt').config()
-  require('cfg.lazy').config()
-end
+require('cfg.yank-highlight').config()
+require('cfg.resume').config()
+
+require('cfg.file-types').config()
+require('cfg.markdown').config()
+
+require('cfg.wt').config()
+
+require('cfg.lazy').config()
