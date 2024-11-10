90bbf1012dc027389e498605e8af129edb4fc14e
greg <grzegorz.kozub@gmail.com>
2024-11-10 06:09:58 +0100

    miminal copilot

---
+++ lua/cfg/copilot.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+function M.config()
+  local copilot_loaded, copilot = pcall(require, 'copilot')
+  if not copilot_loaded then
+    return
+  end
+
+  copilot.setup {
+    filetypes = {
+      cucumber = true,
+      cs = true,
+      dockerfile = true,
+      elixir = true,
+      go = true,
+      gomod = true,
+      html = true,
+      javascript = true,
+      json = true,
+      lua = true,
+      nu = true,
+      ps1 = true,
+      python = true,
+      rust = true,
+      sh = true,
+      sql = true,
+      toml = true,
+      typescript = true,
+      yaml = true,
+      zsh = true,
+      ['*'] = false,
+    },
+    panel = { enabled = false },
+    suggestion = {
+      keymap = {
+        accept = '<tab>',
+        dismiss = '<esc>',
+        next = '<M-]>',
+        prev = '<M-[>',
+      },
+    },
+  }
+
+  require('cfg.util').nmap('<leader>c', ':Copilot attach<cr>')
+end
+
+return M
---
+++ lua/plg/copilot.lua
@@ -0,0 +1,6 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot attach',
+  config = require('cfg.copilot').config,
+  keys = { '<leader>c' },
+}
