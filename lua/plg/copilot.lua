e65cd0aa19a6d0151f5c07bee2730750faf14e62
greg <grzegorz.kozub@gmail.com>
2024-11-13 12:08:21 +0100

    disable copilot

--- lua/plg/copilot.lua
+++ lua/plg/copilot.lua
@@ -1,6 +1,6 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot enable',
-  config = require('cfg.copilot').config,
-  keys = { '<leader>ce' },
+  -- 'zbirenbaum/copilot.lua',
+  -- cmd = 'Copilot enable',
+  -- config = require('cfg.copilot').config,
+  -- keys = { '<leader>ce' },
 }
