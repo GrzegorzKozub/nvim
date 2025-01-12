12c5163b1c91956e79b94ca82539e3a33777f320
greg <grzegorz.kozub@gmail.com>
2025-01-12 07:55:02 +0100

    disable pylint hacks

--- lua/cfg/lint.lua
+++ lua/cfg/lint.lua
@@ -11,27 +11,27 @@ local linters = {
   yaml = { 'yamllint' },
 }
 
-local function site_packages()
-  if vim.fn.has 'win32' == 0 then
-    return {
-      '/usr/lib/python3.11/site-packages',
-      os.getenv 'HOME' .. '/.local/lib/python3.11/site-packages',
-    }
-  else
-    return {
-      os.getenv('APPDATA'):gsub('\\', '/') .. '/Python/Python312/site-packages',
-    }
-  end
-end
+-- local function site_packages()
+--   if vim.fn.has 'win32' == 0 then
+--     return {
+--       '/usr/lib/python3.13/site-packages',
+--       os.getenv 'HOME' .. '/.local/lib/python3.13/site-packages',
+--     }
+--   else
+--     return {
+--       os.getenv('APPDATA'):gsub('\\', '/') .. '/Python/Python312/site-packages',
+--     }
+--   end
+-- end
 
-local function pylint(lint)
-  lint.linters.pylint.args = {
-    '--init-hook',
-    'import sys; sys.path += ["' .. table.concat(site_packages(), '","') .. '"]',
-    '--output-format',
-    'json',
-  }
-end
+-- local function pylint(lint)
+--   lint.linters.pylint.args = {
+--     '--init-hook',
+--     'import sys; sys.path += ["' .. table.concat(site_packages(), '","') .. '"]',
+--     '--output-format',
+--     'json',
+--   }
+-- end
 
 function M.config()
   local lint_loaded, lint = pcall(require, 'lint')
@@ -49,7 +49,7 @@ function M.config()
     group = vim.api.nvim_create_augroup('Lint', { clear = true }),
   })
 
-  pylint(lint)
+  -- pylint(lint)
 end
 
 return M
