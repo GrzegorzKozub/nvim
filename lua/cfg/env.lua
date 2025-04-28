9ad3f682cdf8f5d22aac25b7a61adb85e748dbfa
greg <grzegorz.kozub@gmail.com>
2025-04-28 08:22:23 +0200

    update skipping diagnostics for .env to 0.11

--- lua/cfg/env.lua
+++ lua/cfg/env.lua
@@ -2,18 +2,15 @@ local M = {}
 
 function M.config()
   vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-    pattern = '.env',
+    pattern = '*.env',
     callback = function(args)
       vim.diagnostic.enable(false, { bufnr = args.buf })
-      vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-        function(err, result, ctx, config)
-          if result.uri:sub(-4) == '.env' then
-            return
-          end
-          vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
-        end,
-        {}
-      )
+      vim.lsp.handlers['textDocument/publishDiagnostics'] = function(err, result, ...)
+        if result.uri:sub(-4) == '.env' then
+          return nil
+        end
+        return vim.lsp.diagnostic.on_publish_diagnostics(err, result, ...)
+      end
     end,
     group = vim.api.nvim_create_augroup('Env', { clear = true }),
   })
