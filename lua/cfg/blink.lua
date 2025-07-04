691ef9ff2a27974d6502ac2ef98c2829a9fa5e40
greg <grzegorz.kozub@gmail.com>
2025-07-04 08:24:16 +0200

    more blink

--- lua/cfg/blink.lua
+++ lua/cfg/blink.lua
@@ -5,7 +5,63 @@ function M.config()
   if not blink_loaded then
     return
   end
-  blink.setup()
+
+  -- resume from https://main.cmp.saghen.dev/configuration/appearance.html
+  -- todo: enter & tab to accept, mini.icons cleanup, defaults removal
+  blink.setup {
+    -- keymap = { preset = 'default' },
+    completion = {
+      menu = {
+        border = 'rounded',
+        draw = {
+          -- columns = {
+          -- { 'label', 'label_description', gap = 1 },
+          -- { 'kind_icon', 'kind' },
+          -- },
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                return icon
+              end,
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+        window = { border = 'rounded' },
+      },
+      ghost_text = { enabled = true },
+    },
+    -- signature = { window = { border = 'single' } },
+    signature = { enabled = true },
+    -- sources = {
+    --       default = { 'lsp', 'path', 'snippets', 'buffer' },
+    --     },
+    sources = {
+      providers = {
+        path = {
+          opts = {
+            get_cwd = function(_)
+              return vim.fn.getcwd()
+            end,
+          },
+        },
+      },
+    },
+  }
 end
 
 return M
--- lua/plg/blink.lua
+++ lua/plg/blink.lua
@@ -1,7 +1,8 @@
 return {
   'saghen/blink.cmp',
   config = require('cfg.blink').config,
-  event = { 'CmdlineEnter', 'InsertEnter' },
+  -- event = { 'CmdlineEnter', 'InsertEnter' },
+  event = { 'VeryLazy' }, -- loads 2x faster
   opts_extend = { 'sources.default' },
   version = '1.*',
 }
