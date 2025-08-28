82b0158c5d3bab803f8b20aec13eb2770c26ca38
greg <grzegorz.kozub@gmail.com>
2025-08-28 11:46:05 +0200

    add ctrl+i for signature help, same as zed

--- lua/cfg/blink.lua
+++ lua/cfg/blink.lua
@@ -95,6 +95,7 @@ function M.config()
       ['<s-down>'] = { 'scroll_documentation_down', 'fallback' },
       ['<c-b>'] = false,
       ['<c-f>'] = false,
+      ['<c-i>'] = { 'show_signature', 'hide_signature', 'fallback' }, -- same as zed
       ['<c-s>'] = { 'show_signature', 'hide_signature', 'fallback' }, -- overrides neovim default
       ['<c-k>'] = false,
     },
