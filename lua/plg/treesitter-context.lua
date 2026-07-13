bc76e50b816bdd7b2c5805651ac6dba4338b1473
greg <grzegorz.kozub@gmail.com>
2026-07-13 21:06:07 +0200

    re-enable treesitter on windows

--- lua/plg/treesitter-context.lua
+++ lua/plg/treesitter-context.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
-  cond = vim.fn.has 'win32' == 0,
   config = require('cfg.treesitter-context').config,
   event = { 'BufNewFile', 'BufReadPre' },
 }
--- lua/plg/treesitter.lua
+++ lua/plg/treesitter.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  cond = vim.fn.has 'win32' == 0,
   build = ':TSUpdate',
   cmd = 'TSUpdate',
   config = require('cfg.treesitter').config,
