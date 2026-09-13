8afe16ee95ccaafe4e3fb27aa7c51d764d810be5
greg <grzegorz.kozub@gmail.com>
2026-09-13 06:38:16 +0200

    cleanup zsh-lint

--- lua/cfg/lint.lua
+++ lua/cfg/lint.lua
@@ -16,23 +16,20 @@ if vim.fn.has 'win32' == 0 then
   linters.dockerfile = { 'hadolint' }
   linters.terraform = { 'tflint' }
   linters['yaml.ansible'] = { 'ansible_lint' }
-  linters.zsh = { 'zsh_lint' }
+  linters.zsh = { 'zsh-lint' }
 end
 
-local zsh_lint_severity = {
-  error = vim.diagnostic.severity.ERROR,
-  warning = vim.diagnostic.severity.WARN,
-  info = vim.diagnostic.severity.INFO,
-  hint = vim.diagnostic.severity.HINT,
-}
-
-function M.config()
-  local lint_loaded, lint = pcall(require, 'lint')
-  if not lint_loaded then
+local function zsh_lint(lint)
+  if vim.fn.has 'win32' == 1 then
     return
   end
-
-  lint.linters.zsh_lint = {
+  local severity = {
+    error = vim.diagnostic.severity.ERROR,
+    warning = vim.diagnostic.severity.WARN,
+    info = vim.diagnostic.severity.INFO,
+    hint = vim.diagnostic.severity.HINT,
+  }
+  lint.linters['zsh-lint'] = {
     cmd = 'zsh-lint',
     args = { '--format=json' },
     stdin = false,
@@ -45,19 +42,28 @@ function M.config()
       end
       for _, d in ipairs(decoded.diagnostics) do
         table.insert(diagnostics, {
+          severity = severity[d.severity],
+          source = 'zsh-lint',
           lnum = d.range.start.line - 1,
           col = d.range.start.column - 1,
           end_lnum = d.range['end'].line - 1,
           end_col = d.range['end'].column - 1,
-          severity = zsh_lint_severity[d.severity] or vim.diagnostic.severity.WARN,
           message = d.message,
           code = d.rule,
-          source = 'zsh-lint',
         })
       end
       return diagnostics
     end,
   }
+end
+
+function M.config()
+  local lint_loaded, lint = pcall(require, 'lint')
+  if not lint_loaded then
+    return
+  end
+
+  zsh_lint(lint)
 
   lint.linters_by_ft = linters
 
